6f6d359400e3344d052ec5b8e966e828bda379c6
felops <felops@users.noreply.github.com>
2017-09-29 19:17:30 -0300

    ✨ add option to create select input

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -3,11 +3,26 @@ import { FormGroup, Label, Input } from 'reactstrap';
 
 export default class InputGroup extends Component {
   render() {
-    return (
-      <FormGroup>
-        <Label for={this.props.id}>{this.props.label}</Label>
-        <Input type={this.props.type} name={this.props.name} id={this.props.id} onChange={this.props.onChange}/>
-      </FormGroup>
-    );
+    if(this.props.type == 'select') {
+      const options = this.props.options.map((option, i) =>
+        <option key={option.id}>{option.name}</option>
+      );
+
+      return (
+        <FormGroup>
+          <Label for={this.props.id}>{this.props.label}</Label>
+          <Input type='select' name={this.props.name} id={this.props.id} onChange={this.props.onChange}>
+            {options}
+          </Input>
+        </FormGroup>
+      );
+    } else {
+      return (
+        <FormGroup>
+          <Label for={this.props.id}>{this.props.label}</Label>
+          <Input type={this.props.type} name={this.props.name} id={this.props.id} onChange={this.props.onChange}/>
+        </FormGroup>
+      );
+    }
   }
 }
