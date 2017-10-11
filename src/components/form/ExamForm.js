65125d6cccb04f6ea44886a251ec493a2ac64661
felops <felops@users.noreply.github.com>
2017-10-10 21:23:33 -0300

    ✨ create an exam feature

--- src/components/form/ExamForm.js
+++ src/components/form/ExamForm.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import Form from './Form';
+import axios from 'axios';
 
 const inputs = [
   {
+    type: 'hidden',
+    name: 'professor',
+    id: 'professor',
+    value: 1
+  },
+  {
     type: 'select',
-    name: 'disciplina',
-    id: 'disciplina',
+    name: 'class',
+    id: 'class',
+    label: 'Classe',
+    options: [
+      {id:1, name:'1A'},
+      {id:2, name:'1B'},
+      {id:3, name:'2A'},
+      {id:4, name:'2B'}
+    ]
+  },
+  {
+    type: 'select',
+    name: 'discipline',
+    id: 'discipline',
     label: 'Disciplina',
     options: [
       {id:1, name:'Matemática'},
@@ -43,8 +62,8 @@ const inputs = [
   },
   {
     type: 'select',
-    name: 'font',
-    id: 'font',
+    name: 'source',
+    id: 'source',
     label: 'Fonte',
     options: [
       {id:1, name:'ENEM'},
@@ -56,20 +75,26 @@ const inputs = [
     type: 'number',
     name: 'questions',
     id: 'questions',
-    label: 'Número de Questões',
-    options: [
-      {id:1, name:'ENEM'},
-      {id:2, name:'ETEC'},
-      {id:3, name:'FATEC'},
-    ]
+    label: 'Número de Questões'
   }
 ];
 
 export default class ExamForm extends Component {
+  onClick(e, state) {
+    let data = {};
+    for(let item in state) {
+      data[item] = state[item];
+    }
+
+    axios.post('http://localhost:3000/api/exam', data).then((response) =>
+      console.log(response)
+    );
+  }
+
   render() {
     return (
       <div>
-        <Form inputs={inputs}/>
+        <Form inputs={inputs} onClick={this.onClick}/>
       </div>
     );
   }
