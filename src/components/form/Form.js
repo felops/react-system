bd2b5be9b39b9ca0a985e27ee2720defb0df79be
felops <felops@users.noreply.github.com>
2017-10-11 13:14:56 -0300

    🐛 handle error when the component 'Form' doesn't receive props

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -5,9 +5,12 @@ import { Button } from 'reactstrap';
 export default class Form extends Component {
   componentDidMount() {
     let state = {};
-    this.props.inputs.map(function(input) {
-      return state[input.name] = input.value;
-    });
+
+    if(this.props.inputs) {
+      this.props.inputs.map(function(input) {
+        return state[input.name] = input.value;
+      });
+    }
 
     this.setState(state);
     this.handleChange = this.handleChange.bind(this);
@@ -26,9 +29,13 @@ export default class Form extends Component {
 
   render() {
     let button;
-    let inputForms = this.props.inputs.map((input, i) =>
-      <InputForm {...input} key={'a' + i} onChange={this.handleChange}/>
-    );
+    let inputForms;
+
+    if(this.props.inputs) {
+      inputForms = this.props.inputs.map((input, i) =>
+        <InputForm {...input} key={'a' + i} onChange={this.handleChange}/>
+      );
+    }
 
     if(this.props.onClick) {
       button = <Button color="primary" onClick={this.onClick}>Criar</Button>;
