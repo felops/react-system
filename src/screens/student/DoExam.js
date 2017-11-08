1105b06c2d59d757ae817648c83a8ed4e23d8a7e
felops <felops@users.noreply.github.com>
2017-11-08 01:44:47 -0200

    ✨ DoExam with step by step question and validation

--- src/screens/student/DoExam.js
+++ src/screens/student/DoExam.js
@@ -1,13 +1,135 @@
-import React, { Component } from 'react';
-import Exam from './../../components/Exam';
+import React, { Component } from 'react'
+import axios from 'axios'
+import ExamQuestion from './../../components/ExamQuestion'
+
+import 'rc-steps/assets/index.css'
+import 'rc-steps/assets/iconfont.css'
+import Steps, { Step } from 'rc-steps'
 
 export default class DoExam extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      step: 0,
+      error: '',
+      questions: null,
+      responses: {},
+      isFinished: false,
+      currentQuestion: null
+    }
+  }
+
+  buttonClick(e) {
+    const state = this.state
+
+    if(state.responses[state.currentQuestion]) {
+      axios.post('http://localhost:3000/api/examStudent', {
+        student: 1,
+        examQuestion: state.currentQuestion,
+        questionOption: state.responses[state.currentQuestion]
+      }).then((response) => {
+        let nextStep = state.step + 1
+        if(nextStep < state.questions.length) {
+          this.setState({
+            currentQuestion: state.questions[nextStep].id,
+            step: nextStep
+          })
+        } else {
+          this.setState({ isFinished: true })
+        }
+      })
+    } else {
+      this.setState({ error: 'Selecione uma resposta antes de prosseguir.' })
+    }
+  }
+
+  handleRadioChange(e) {
+    const question = e.target.name.substr(1)
+
+    this.setState({
+      error: '',
+      responses: {
+        ...this.state.responses,
+        [question]: e.target.value
+      },
+      currentQuestion: parseInt(question),
+    })
+  }
+
+  formify(questions) {
+    let arrLength = questions.length
+
+    return questions.map((question, i) => {
+      question.Question.QuestionOptions.map((option) => {
+        option.type = 'radio'
+        option.name = 'q' + question.id
+        option.value = option.id
+        option.description = option.option
+        return option
+      })
+
+      let text = arrLength === i+1 ? 'Salvar e finalizar' : 'Salvar e prosseguir'
+      question.button = {
+        onClick: this.buttonClick.bind(this),
+        text: text
+      }
+
+      return question
+    })
+  }
+
+  componentWillMount() {
+    let exam = this.props.match.params.id
+
+    if(exam) {
+      axios.get('http://localhost:3000/api/loadExam/' + exam).then((response) => {
+        this.setState({
+          questions: this.formify(response.data),
+          currentQuestion: response.data[0].id
+        })
+      })
+    }
+  }
+
+  renderError() {
+    if (this.state.error) {
+      return <div className="alert alert-danger" role="alert">{this.state.error}</div>
+    }
+
+    return null
+  }
+
   render() {
-    return (
-      <div>
-        <h3>Avaliação</h3>
-        <Exam exam={this.props.match.params.id}/>
-      </div>
-    );
+      const state = this.state,
+            isFinished = state.isFinished,
+            questions = state.questions,
+            step = state.step
+
+      if(isFinished) {
+        return (
+          <div>
+            <h5>Respostas enviadas!</h5>
+            <p>Aguarde o final do exame para consultar seu desempenho.</p>
+          </div>
+        )
+      } else if(questions) {
+        let steps = questions.map((question, i) => <Step key={i}/>)
+
+        return (
+          <div>
+            <Steps current={this.state.step}>
+              {steps}
+            </Steps>
+            {this.renderError()}
+            <ExamQuestion
+              key={questions[step].id}
+              {...questions[step]}
+              title={'Questão ' + (step + 1)}
+              handleChange={this.handleRadioChange.bind(this)} />
+          </div>
+        )
+      } else {
+        return <p className='text-muted'>carregando..</p>
+      }
   }
 }
